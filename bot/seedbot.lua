363d6ebdf9ab4ef77a2e7a2462db2c6ce320670a
m2fan <2fani.clash@gmail.com>
2016-01-22 02:46:55 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,29 +221,18 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {131189448,146661928},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+    about_text = [[Fox v2 😊
+👥 Sudos: 
+👤 @Tofaniyam 
+👤 @teleadminfox 
+👥 Admins:
+.........
+And soon we will have a new director😱
+📢Channal: @foxchannal
+Have fun.....❤️
 ]],
     help_text_realm = [[
 Realm Commands:
