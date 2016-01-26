c8b8c005f7b39ed10dd3aa2bc86755a6a63e929c
m2fan <2fani.clash@gmail.com>
2016-01-27 00:32:23 +0330

    Update inrealm.lua

--- plugins/inrealm.lua
+++ plugins/inrealm.lua
@@ -4,7 +4,7 @@ do
  
 local function create_group(msg)
         -- superuser and admins only (because sudo are always has privilege)
-        if is_sudo(msg)and is_admin(msg) then
+        if is_sudo(msg) and is_admin(msg) then
                 local group_creator = msg.from.print_name
         create_group_chat (group_creator, group_name, ok_cb, false)
                 return 'Group '..string.gsub(group_name, '_', ' ')..' has been created.'
