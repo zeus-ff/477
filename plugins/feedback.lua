16ac9cea7a85f1e6c945d168b73473b97226847a
ehsannbt <ehsan.gol80@yahoo.com>
2016-01-29 08:01:04 +0000

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -1,53 +1,27 @@
 do
-
  function run(msg, matches)
-
  
-
-  local fuse = '📌 New #feedback\n\n👤 ID : ' .. msg.from.id .. '\n\n🔍 Name: ' .. msg.from.print_name ..'\n\n🈯️ Username: @' .. msg.from.username .. '\n\n 📝 The Pm:\n' .. matches[1] 
-
+ local fuse = 'New FeedBack\n\nId : ' .. msg.from.id .. '\n\nName: ' .. msg.from.print_name ..'\n\nUsername: @' .. msg.from.username .. '\n\nThe Pm:\n' .. matches[1] 
  local fuses = '!printf user#id' .. msg.from.id
-
  
-
  
-
    local text = matches[1]
-
-   local chat = "chat#id"..105222172 
-
+   local chat = "chat#id"..YourChatId 
    --like : local chat = "chat#id"..12345678
-
    
-
   local sends = send_msg(chat, fuse, ok_cb, false)
-
-  return '✅ پیغام شما ارسال شد ✅'
-
+  return 'Sent!'
  
-
  end
-
  end
-
  return {
-
   
-
   description = "Feedback",
-
  
-
   usage = "!feedback message",
-
   patterns = {
-
   "^[!/][Ff]eedback (.*)$"
-
  
-
   },
-
   run = run
-
  }
