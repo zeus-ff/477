22e3f7e6fef145e3cc8219fa82f76b5ba022cf33
m2fan <2fani.clash@gmail.com>
2016-02-05 00:07:06 +0330

    Update welcome.lua

--- plugins/welcome.lua
+++ plugins/welcome.lua
@@ -1,104 +1,136 @@
-local add_user_cfg = load_from_file('data/add_user_cfg.lua')
+--[[
+Sends a custom message when a user enters or leave a chat.
+!welcome group
+The custom message will send to the group. Recommended way.
+!welcome pm
+The custom message will send to private chat newly joins member.
+Not recommended as a privacy concern and the possibility of user reporting the bot.
+!welcome disable
+Disable welcome service. Also, you can just disable greeter plugin.
+--]]
 
-local function template_add_user(base, to_username, from_username, chat_name, chat_id)
-   base = base or ''
-   to_username = '@' .. (to_username or '')
-   from_username = '@' .. (from_username or '')
-   chat_name = string.gsub(chat_name, '_', ' ') or ''
-   chat_id = "chat#id" .. (chat_id or '')
-   if to_username == "@" then
-      to_username = ''
-   end
-   if from_username == "@" then
-      from_username = ''
-   end
-   base = string.gsub(base, "{to_username}", to_username)
-   base = string.gsub(base, "{from_username}", from_username)
-   base = string.gsub(base, "{chat_name}", chat_name)
-   base = string.gsub(base, "{chat_id}", chat_id)
-   return base
+do
+
+local function is_banned(user_id, chat_id)
+  local hash =  'banned:'..chat_id..':'..user_id
+  local banned = redis:get(hash)
+  return banned or false
 end
 
-function chat_new_user_link(msg)
-   local pattern = add_user_cfg.initial_chat_msg
-   local to_username = msg.from.username
-   local from_username = 'link (@' .. (msg.action.link_issuer.username or '') .. ')'
-   local chat_name = msg.to.print_name
-   local chat_id = msg.to.id
-   pattern = template_add_user(pattern, to_username, from_username, chat_name, chat_id)
-   if pattern ~= '' then
-      local receiver = get_receiver(msg)
-      send_msg(receiver, pattern, ok_cb, false)
-   end
+local function is_super_banned(user_id)
+    local hash = 'superbanned:'..user_id
+    local superbanned = redis:get(hash)
+    return superbanned or false
 end
 
-function chat_new_user(msg)
-   local pattern = add_user_cfg.initial_chat_msg
-   local to_username = msg.action.user.username
-   local from_username = msg.from.username
-   local chat_name = msg.to.print_name
-   local chat_id = msg.to.id
-   pattern = template_add_user(pattern, to_username, from_username, chat_name, chat_id)
-   if pattern ~= '' then
-      local receiver = get_receiver(msg)
-      send_msg(receiver, pattern, ok_cb, false)
-   end
-end
+local function welcome_message(msg, new_member)
 
-local function description_rules(msg, nama)
-   local data = load_data(_config.moderation.data)
-   if data[tostring(msg.to.id)] then
-      local about = ""
-      local rules = ""
-      if data[tostring(msg.to.id)]["description"] then
-         about = data[tostring(msg.to.id)]["description"]
-         about = "\nAbout :\n"..about.."\n"
-      end
-      if data[tostring(msg.to.id)]["rules"] then
-         rules = data[tostring(msg.to.id)]["rules"]
-         rules = "\nRules :\n"..rules.."\n"
-      end
-    local sambutan = "Hi "..nama.."\nWelcome to '"..string.gsub(msg.to.print_name, "_", " ").."'\nYou can use help for see bot commands\n"
-      local text = sambutan..about..rules.."\n"
-      local receiver = get_receiver(msg)
-      send_large_msg(receiver, text, ok_cb, false)
-   end
+  local data = load_data(_config.moderation.data)
+  local welcome_stat = data[tostring(msg.to.id)]['settings']['welcome']
+
+  if data[tostring(msg.to.id)] then
+    local about = ''
+    local rules = ''
+    if data[tostring(msg.to.id)]['description'] then
+      about = data[tostring(msg.to.id)]['description']
+      about = "\nDescription :\n"..about.."\n"
+    end
+    if data[tostring(msg.to.id)]['rules'] then
+      rules = data[tostring(msg.to.id)]['rules']
+      rules = "\nRules :\n"..rules.."\n"
+    end
+    local welcomes = "Welcome "..new_member..".\nYou are in group '"..string.gsub(msg.to.print_name, "_", " ").."'\n"
+    if welcome_stat == 'group' then
+      receiver = get_receiver(msg)
+    elseif welcome_stat == 'private' then
+      receiver = 'user#id'..msg.from.id
+    end
+    send_large_msg(receiver, welcomes..about..rules.."\n", ok_cb, false)
+  end
 end
 
 local function run(msg, matches)
-   if not msg.service then
-      return "Are you trying to troll me?"
-   end
-   --vardump(msg)
-   if matches[1] == "chat_add_user" then
-      if not msg.action.user.username then
-          nama = string.gsub(msg.action.user.print_name, "_", " ")
-      else 
-          nama = "@"..msg.action.user.username
+
+  local data = load_data(_config.moderation.data)
+  local welcome_stat = data[tostring(msg.to.id)]['settings']['welcome']
+
+  if matches[1] == 'welcome' then
+    if matches[2] == 'group' then
+      if welcome_stat ~= 'group' then
+        data[tostring(msg.to.id)]['settings']['welcome'] = 'group'
+        save_data(_config.moderation.data, data)
       end
-      chat_new_user(msg)
-      description_rules(msg, nama)
-   elseif matches[1] == "chat_add_user_link" then
-      if not msg.from.username then
-          nama = string.gsub(msg.from.print_name, "_", " ")
+      return 'Welcome service already enabled.\nWelcome message will shown in group.'
+    end
+    if matches[2] == 'pm' then
+      if welcome_stat ~= 'private' then
+        data[tostring(msg.to.id)]['settings']['welcome'] = 'private'
+        save_data(_config.moderation.data, data)
+      end
+      return 'Welcome service already enabled.\nWelcome message will send as private message to new member.'
+    end
+    if matches[2] == 'disable' then
+      if welcome_stat == 'no' then
+        return 'Welcome service is not enabled.'
       else
-          nama = "@"..msg.from.username
+        data[tostring(msg.to.id)]['settings']['welcome'] = 'no'
+        save_data(_config.moderation.data, data)
+        return 'Welcome service has been disabled.'
       end
-      chat_new_user_link(msg)
-      description_rules(msg, nama)
-   elseif matches[1] == "chat_del_user" then
-       local bye_name = msg.action.user.first_name
-       return 'Bye '..bye_name
-   end
+    end
+  end
+
+  if welcome_stat ~= 'no' and msg.action and msg.action.type then
+    -- do not greet (super)banned users or API bots.
+    local action = msg.action.type
+    if action == 'chat_add_user' or action == 'chat_add_user_link' or action == "chat_del_user" then
+      if msg.action.link_issuer then
+        user_id = msg.from.id
+        new_member = (msg.from.first_name or '')..' '..(msg.from.last_name or '')
+        user_flags = msg.flags
+      else
+	      user_id = msg.action.user.id
+        new_member = (msg.action.user.first_name or '')..' '..(msg.action.user.last_name or '')
+        user_flags = msg.action.user.flags
+      end
+      local superbanned = is_super_banned(user_id)
+      local banned = is_banned(user_id, msg.to.id)
+      if superbanned or banned then
+        print 'Ignored. User is banned!'
+        return nil
+      end
+      if user_flags == 4352 then
+        print "Ignored. It's an API bot."
+        return nil
+      end
+    end
+
+    if matches[1] == "chat_add_user" then
+      welcome_message(msg, new_member)
+    elseif matches[1] == "chat_add_user_link" then
+      welcome_message(msg, new_member)
+    elseif matches[1] == "chat_del_user" then
+      return 'Bye '..new_member..'!'
+    end
+  end
+
 end
 
 return {
-   description = "Welcoming Message",
-   usage = "send message to new member",
-   patterns = {
-      "^!!tgservice (chat_add_user)$",
-      "^!!tgservice (chat_add_user_link)$",
-      "^!!tgservice (chat_del_user)$",
-   },
-   run = run
+  description = 'Sends a custom message when a user enters or leave a chat.',
+  usage = {
+    moderator = {
+      '!welcome group : Welcome message will shows in group.',
+      '!welcome pm : Welcome message will send to new member via PM.',
+      '!welcome disable : Disable welcome message.'
+    },
+  },
+  patterns = {
+    "^!!tgservice (.+)$",
+    "^[!/](welcome) (.*)$"
+  },
+  run = run,
+  moderated = true
 }
+
+end
