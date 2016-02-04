5dd593e161ce87f38ec99187255711341aa864b7
m2fan <2fani.clash@gmail.com>
2016-02-04 23:40:58 +0330

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -14,11 +14,6 @@ local function action_by_reply(extra, success, result)
   else
     user_name = ''
   end
-  local msg = result
-  local user_id = msg.from.id
-  local chat_id = msg.to.id
-  local user = 'user#id'..msg.from.id
-  local chat = 'chat#id'..msg.to.id
   local data = load_data(_config.moderation.data)
   if data[tostring('admins')][tostring(user_id)] then
     who = 'Admim'
@@ -40,14 +35,6 @@ local function action_by_reply(extra, success, result)
              Position in group : '..who
   send_large_msg(extra.receiver, text)
 end
-
-local function run(msg, matches)
-   if matches[1] == 'info' and msg.reply_id then
-     get_message(msg.reply_id, action_by_reply, {receiver=get_receiver(msg)})
-   end
-end
-
-return {
     patterns = {
       "^me$"
     },
